41904ed83a844d7fdaa8a74c3ab06c8e4fc1fffa
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-02-21 12:04:29 +0530

    create card layout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var today = new Date();
 
-function date_format(today) {
+function dateFormat(today) {
     var date = "";
     date += today.getFullYear();
     date += '-';
@@ -10,28 +10,39 @@ function date_format(today) {
     return date;
 }
 
-function date_subtract() {
+function dateSubtract() {
     today.setDate(today.getDate() - 1);
-    return date_format(today);
+    return dateFormat(today);
 }
 
 var url = "https://api.nasa.gov/planetary/apod\?api_key\=fWGvSVshJd95FFVtTFPMEbIniRM1UrTULMIOkHvM\&date\="
 
+function createCard(myJson) {
+    var card = document.createElement('div');
+    card.class = 'card';
+    
+    var date = document.createElement('p');
+    date.innerHTML = myJson.date;
+    card.appendChild(date);
+
+    var img = document.createElement('img');
+    img.src = myJson.url;
+    card.appendChild(img);
+    
+    return card;
+}
+
 while(today.getFullYear()  == 2019) {
-    var date = date_format(today);
+    var date = dateFormat(today);
 
     fetch(url+date)
     .then(function(response) {
         return response.json();
     })
     .then(function(myJson) {
-        console.log("SDg");
-        
         console.log(JSON.stringify(myJson));
-        var main = document.getElementById('main');
-        var img = document.createElement('img');
-        img.src = myJson.url;
-        main.appendChild(img);
+        var main = document.getElementById('container');
+        main.appendChild(createCard(myJson));
         //append instead of replace
         
     }).catch(function(error) {
@@ -39,5 +50,5 @@ while(today.getFullYear()  == 2019) {
         
     });
 
-    date = date_subtract();
+    date = dateSubtract();
 }
